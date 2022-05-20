f55992f0519d7f2db4ccdefbe70a56594bbee39d
Shatabdi <u1404023@student.cuet.ac.bd>
2022-05-20 23:32:03 +0600

    initial commit

---
+++ includes/scripts.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     $("#CH").click(function () {
+//         $(".nav-link").removeClass("active");
+//         // $(".tab").addClass("active"); // instead of this do the below 
+//         $(this).addClass("active");   
+//     });
+//     });
